a10f579178562f00dcb96be185a54528193a7279
José <josedaniel.alvaradof@gmail.com>
2022-04-21 14:55:22 -0600

    operaciones con publicaciones

--- Backend/routes/Publicaciones.js
+++ Backend/routes/Publicaciones.js
@@ -8,21 +8,40 @@ router.use(cors({ origin: true, optionsSuccessStatus: 200 }));
 router.use(bodyParser.json({ limit: "50mb", extended: true }));
 router.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-router.post("/crearPublicacion", async function (req, res) {
-  //const {descripcion, fecha, id_clase} = req.body   
-  /*
-  let resConsulta = await service.consultar(
-    `SHOW TABLES;`,
-  );
-*/
-  service.consultar(`SELECT * FROM curso;`, function (result) {
-    console.log('Data : ' + result[0]);
-    res.status(200).json({ datoos: result });
-  });
-  //console.log(resConsulta);
 
-  
-  //res.send(resConsulta)
+router.post("/createPublicacion", async function (req, res) {
+  const {descripcion, fecha, id_clase} = req.body   
+  let consulta = `CALL create_publicacion('${descripcion}','${fecha}', ${id_clase});`;
+  service.consultar(consulta, function (result) {
+    res.status(200).json({ datos: result });
+  });
+});
+
+
+router.get("/getPublicaciones", async function (req, res) {
+  const {id_publicacion} = req.body 
+  let consulta = `SELECT * FROM publicacion;`;
+  service.consultar(consulta, function (result) {
+    res.status(200).json(result);
+  });
+});
+
+
+router.get("/updatePublicacion", async function (req, res) {
+  const {id_publicacion, descripcion, fecha, id_clase} = req.body 
+  let consulta = `CALL update_publicacion(${id_publicacion}, '${descripcion}', '${fecha}', ${id_clase})`;
+  service.consultar(consulta, function (result) {
+    res.status(200).json(result);
+  });
+});
+
+
+router.get("/deletePublicacion", async function (req, res) {
+  const {id_publicacion} = req.body 
+  let consulta = `CALL delete_publicacion(${id_publicacion});`;
+  service.consultar(consulta, function (result) {
+    res.status(200).json(result);
+  });
 });
 
 module.exports = router;
--- Backend/routes/connection.js
+++ Backend/routes/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "201909103",
+  password: "12345",
   database: 'proyecto_lab'
 });
 
@@ -11,19 +11,13 @@ async function consultar(consulta, callback) {
   try {
     connection.query(consulta, async function (err, result) {
       if (err) {
-        console.log(err.message);
         callback({ "status": 400, "message": err.message });
-        return { "status": 400, "message": err.message };
       } else {
         callback({ "status": 200, "message": result });
-        return { "status": 200, "message": result };
       }
     });
-
-    callback();
-    
   } catch (error) {
-    return { "status": 400, "message": error }
+    console.log({"status": 400, "message": error})
   }
 }
 
