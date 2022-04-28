46c816b9737b7bd0aafee965dd07602543218037
José <josedaniel.alvaradof@gmail.com>
2022-04-28 13:35:45 -0600

    procedimientos de maestro

--- Scripts/consultas.sql
+++ Scripts/consultas.sql
@@ -1,25 +1,61 @@
 USE proyecto_lab;
 
--- Publicaciones para un maestro
+-- __________________________________ Publicaciones ___________________________________________________
+
+-- Obtener publicaicones de maestro por curso
 SELECT *
 FROM clase
-INNER JOIN publicacion USING(id_clase)
+INNER JOIN publicacion USING (id_clase)
+INNER JOIN curso USING (id_curso)
 WHERE (clase.id_curso = 4 AND clase.id_maestro = 449);
- 
 
--- Publicaciones para un alumnos
+-- Publicaciones hechas para un alumnos
 SELECT 
 	publicacion.id_publicacion,
 	publicacion.descripcion,
 	publicacion.fecha,
 	publicacion.id_clase
 FROM alumno
-INNER JOIN asignacion_clase USING(id_alumno)
-INNER JOIN clase USING(id_clase)
+INNER JOIN asignacion_clase USING (id_alumno)
+INNER JOIN clase USING (id_clase)
 INNER JOIN publicacion  USING (id_clase)
 WHERE alumno.id_alumno = 1;
 
--- Publicaciones para un alumnos
+-- Obtener los cursos que imparte un maestro 
+SELECT 
+curso.id_curso,
+curso.nombre_curso 
+FROM maestro
+INNER JOIN clase USING(id_maestro)
+INNER JOIN curso USING(id_curso)
+WHERE maestro.id_maestro = 1705;
+
+-- __________________________________ Actividades ___________________________________________________
+
+-- Obtener las actividad hechas por un maestro por curso
+SELECT 
+actividad.id_actividad,
+actividad.titulo,
+actividad.descripcion,
+actividad.fecha_publicacion,
+actividad.fecha_entrega,
+actividad.valor,
+clase.id_clase 
+FROM maestro
+INNER JOIN clase USING (id_maestro)
+INNER JOIN actividad USING (id_clase)
+WHERE (maestro.id_maestro = 179 AND clase.id_clase = 45);
+
+
+-- Obtener actividades en las que ha sido asignado un alumno 
+SELECT 
+*
+FROM alumno
+INNER JOIN asignacion_actividad USING ()
+
+
+
+
 
 
 
---
+++ Scripts/procedimientos_maestros.sql
@@ -0,0 +1,232 @@
+USE proyecto_lab;
+
+-- ___________________________ Obetener maestro ___________________________
+CREATE PROCEDURE get_maestro (
+	IN id_maestro INT
+)
+BEGIN 
+	SELECT * FROM maestro WHERE maestro.id_maestro = id_maestro;
+END;
+
+DROP PROCEDURE IF EXISTS get_maestro;
+CALL get_maestro(5);
+
+-- ___________________________ Obetener publicaciones ___________________________
+CREATE PROCEDURE get_publicaciones_maestro (
+	IN id_maestro INT
+)
+BEGIN 
+	SELECT *
+	FROM clase
+	INNER JOIN publicacion USING (id_clase)
+	INNER JOIN curso USING (id_curso)
+	WHERE clase.id_maestro = id_maestro;	
+END;
+
+DROP PROCEDURE IF EXISTS get_publicaciones_maestro;
+CALL get_publicaciones_maestro(449);
+
+-- ___________________________ Obetener publicaciones ___________________________
+CREATE PROCEDURE get_curso_maestro (
+	IN id_maestro INT
+)
+BEGIN 
+    SELECT * FROM clase
+    INNER JOIN curso USING (id_curso) 
+    WHERE clase.id_maestro = id_maestro;
+END;
+
+DROP PROCEDURE IF EXISTS get_curso_maestro;
+CALL get_curso_maestro(449);
+
+-- ___________________________ Obetener id de clase ___________________________
+CREATE PROCEDURE get_id_clase (
+	IN id_maestro INT,
+	IN id_curso INT
+)
+BEGIN 
+    SELECT id_clase 
+    FROM clase
+    INNER JOIN curso USING (id_curso) 
+    WHERE clase.id_maestro = id_maestro AND clase.id_curso = id_curso;
+END;
+
+DROP PROCEDURE IF EXISTS get_id_clase;
+CALL get_id_clase(173, 4);
+
+-- ___________________________ Crear publicacion ___________________________
+CREATE PROCEDURE create_publicacion (
+	IN descripcion VARCHAR(250), 
+	IN id_clase INT
+)
+BEGIN
+	INSERT INTO publicacion
+	(descripcion, fecha, id_clase)
+	VALUES (descripcion, now(), id_clase);
+END;
+
+DROP PROCEDURE IF EXISTS create_publicacion;
+CALL create_publicacion('descripcion', 1);
+
+-- ___________________________ Obetener publicacion de maestro ___________________________
+CREATE PROCEDURE get_publicacion_maestro  (
+	IN id_publicacion INT
+)
+BEGIN
+    SELECT *
+    FROM clase
+    INNER JOIN publicacion USING (id_clase)
+    INNER JOIN curso USING (id_curso)
+    WHERE publicacion.id_publicacion = id_publicacion;
+END;
+
+DROP PROCEDURE IF EXISTS get_publicacion_maestro;
+CALL get_publicacion_maestro(1);
+
+
+-- ___________________________ Actualizar publicacion ___________________________
+CREATE PROCEDURE update_publicacion (
+	IN id_publicacion INT, 
+	IN descripcion VARCHAR(250)
+)
+BEGIN 
+    UPDATE publicacion 
+    SET 
+    publicacion.descripcion = descripcion,
+    publicacion.fecha = CURDATE()
+    WHERE publicacion.id_publicacion = id_publicacion;
+END;
+
+DROP PROCEDURE IF EXISTS update_publicacion;
+CALL update_publicacion(1, 'descripcion nueva');
+
+SELECT * FROM publicacion;
+
+-- ___________________________ Borrar publicacion ___________________________
+CREATE PROCEDURE delete_publicacion (
+	IN id_publucacion INT
+)
+BEGIN 
+	DELETE FROM publicacion WHERE id_publucacion = id_publucacion;
+END;
+
+DROP PROCEDURE IF EXISTS delete_publicacion;
+CALL delete_publicacion(1);
+
+SELECT * FROM publicacion;
+
+
+-- ___________________________ Obtener actividad de maestro ___________________________
+CREATE PROCEDURE get_actividades_maestro (
+	IN id_maestro INT
+)
+BEGIN 
+  SELECT * FROM clase
+  INNER JOIN actividad USING (id_clase)
+  INNER JOIN curso USING (id_curso)
+  WHERE clase.id_maestro = id_maestro;
+END;
+
+DROP PROCEDURE IF EXISTS get_actividades_maestro;
+CALL get_actividades_maestro(179);
+
+-- ___________________________ insertar actividad ___________________________
+CREATE PROCEDURE insert_actividad (
+	IN titulo VARCHAR(250),
+	IN descripcion VARCHAR(250),
+	IN fecha_entrega DATETIME, 
+	IN valor INT, 
+	IN id_clase INT
+)
+BEGIN 
+  INSERT INTO actividad(titulo, descripcion, fecha_publicacion, fecha_entrega, valor, id_clase)
+  VALUES(titulo, descripcion, CURDATE(), fecha_entrega, valor, id_clase);
+END;
+
+DROP PROCEDURE IF EXISTS insert_actividad;
+CALL insert_actividad('titulo', 'descripcion', NOW(), 100, 1);
+
+
+-- ___________________________ insertar actividad ___________________________
+CREATE PROCEDURE insert_asignacion_actividad (
+	IN estado_actividad VARCHAR(250),
+	IN id_actividad INT, 
+	IN id_alumno INT
+)
+BEGIN 
+	INSERT INTO asignacion_actividad (estado_actividad, id_actividad, id_alumno)
+	VALUES(estado_actividad, id_actividad, id_alumno);
+END;
+
+DROP PROCEDURE IF EXISTS insert_asignacion_actividad;
+CALL insert_asignacion_actividad("Pendiente", 1, 1);
+
+-- ___________________________ get actividad maestro ___________________________
+CREATE PROCEDURE update_actividad_maestro (
+	IN id_actividad INT
+)
+BEGIN 
+    SELECT *
+    FROM clase
+    INNER JOIN actividad USING (id_clase)
+    INNER JOIN curso USING (id_curso)
+    WHERE actividad.id_actividad = id_actividad;
+END;
+
+DROP PROCEDURE IF EXISTS get_actividad_maestro;
+CALL get_actividad_maestro(1);
+
+
+-- ___________________________ update actividad maestro ___________________________
+CREATE PROCEDURE update_actividad_maestro (
+	IN titulo VARCHAR(250),
+	IN descripcion VARCHAR(250),
+	IN fecha_entrega DATETIME, 
+	IN valor INT, 
+	IN id_actividad INT
+)
+BEGIN 
+	UPDATE actividad 
+	SET actividad.titulo = titulo, actividad.descripcion = descripcion ,
+ 	actividad.fecha_publicacion = CURDATE(), actividad.fecha_entrega = fecha_entrega,
+ 	actividad.valor = valor
+ 	WHERE actividad.id_actividad = id_actividad;
+END;
+
+DROP PROCEDURE IF EXISTS update_actividad_maestro;
+CALL update_actividad_maestro('titulo', 'descripcion', NOW(), 100, 1);
+
+
+-- ___________________________ borrar actividad maestro ___________________________
+CREATE PROCEDURE delete_actividad (
+	IN id_actividad INT
+)
+BEGIN 
+	DELETE FROM actividad WHERE actividad.id_actividad = id_actividad;
+END;
+
+DROP PROCEDURE IF EXISTS delete_actividad;
+CALL delete_actividad(100);
+
+
+-- ___________________________ obtener curso de alumno ___________________________
+CREATE PROCEDURE get_alumno_curso (
+	IN id_maestro INT,
+	IN id_curso INT
+)
+BEGIN 
+	SELECT * FROM alumno
+    INNER JOIN asignacion_clase USING (id_alumno)
+    INNER JOIN clase USING (id_clase)
+    WHERE (clase.id_maestro = id_maestro)
+    AND (clase.id_curso= id_curso);
+END;
+
+DROP PROCEDURE IF EXISTS get_alumno_curso;
+CALL get_alumno_curso(1705, 3);
+
+
+
+
+
+
