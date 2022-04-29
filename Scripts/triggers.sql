c95a42bd13c02c856c7ef88a0c0369d6e06e5d53
José <josedaniel.alvaradof@gmail.com>
2022-04-28 18:02:10 -0600

    se agregaron triggers

---
+++ Scripts/triggers.sql
@@ -0,0 +1,37 @@
+USE proyecto_lab;
+
+-- ________________________________ notificar publicacion _________________________________________
+CREATE TRIGGER notificar_publicacion
+AFTER INSERT ON publicacion
+FOR EACH ROW
+BEGIN
+	INSERT INTO notificacion 
+	(titulo, contenido, fecha_hora, id_publicacion)
+	VALUES ('Publicacion realizada', 'Tienes una publicacion por ver', SYSDATE(), NEW.id_publicacion);
+	
+END;
+
+
+-- ________________________________ notificar actividad _________________________________________
+CREATE TRIGGER notificar_publicacion
+AFTER INSERT ON actividad 
+FOR EACH ROW
+BEGIN
+	INSERT INTO notificacion 
+	(titulo, contenido, fecha_hora, id_actividad)
+	VALUES ('Actividad publicada', 'Tienes una actividad por ver', SYSDATE(), NEW.id_publicacion);
+	
+END;
+
+
+-- ________________________________ notificar examen _________________________________________
+CREATE TRIGGER notificar_publicacion
+AFTER INSERT ON examen
+FOR EACH ROW
+BEGIN
+	INSERT INTO notificacion 
+	(titulo, contenido, fecha_hora, id_examen)
+	VALUES ('Examen publicado', 'Tienes una publicacion por ver', SYSDATE(), NEW.id_publicacion);
+	
+END;
+
